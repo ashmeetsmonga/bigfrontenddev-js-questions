adbe6e43f98c9848c1542f7cb14a7df0ae8cfb3e
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-16 21:09:20 +0530

    curry function

---
+++ Curry.js
@@ -0,0 +1,8 @@
+function curry(fn) {
+	return function curried(...args) {
+		if (args.length >= fn.length) return fn(...args);
+		return function (...args2) {
+			return curried(...args, ...args2);
+		};
+	};
+}
