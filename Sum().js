22a5bb5219095a510449731a334e16a3368eb7fc
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-21 16:22:14 +0530

    sum()()

---
+++ Sum().js
@@ -0,0 +1,16 @@
+// Create a sum(), which makes following possible
+
+// const sum1 = sum(1)
+// sum1(2) == 3 // true
+// sum1(3) == 4 // true
+// sum(1)(2)(3) == 6 // true
+// sum(5)(-1)(2) == 6 // true
+
+function sum(num) {
+	const func = (num2) => {
+		return num2 ? sum(num + num2) : num;
+	};
+
+	func.valueOf = () => num;
+	return func;
+}
